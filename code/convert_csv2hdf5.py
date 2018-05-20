7a589d5dd38b56f6032817cd6454900292a349cf
RichardMeyes <richard.meyes@ima.rwth-aachen.de>
2018-05-20 11:55:12 +0200

    added header information to the hdf5 files

--- code/convert_csv2hdf5.py
+++ code/convert_csv2hdf5.py
@@ -1,5 +1,6 @@
 import os
 import h5py
+import numpy as np
 import pandas as pd
 
 
@@ -30,10 +31,28 @@ if __name__ == "__main__":
             print("hdf5 file already exists")
 
         # parse .csv data to hdf5 file
+        print("parsing header information to hdf5 file")
+        # define number of different data files
+        if data_folder_path == train_data_folder_path:
+            data_chars = ["cells", "hits", "particles", "truth"]
+            k = len(data_chars)
+        elif data_folder_path == test_data_folder_path:
+            data_chars = ["cells", "hits"]
+            k = len(data_chars)
+
+        header_info = list()
+        with h5py.File(fp_hdf5, "r+") as f_hdf5:
+            for i, file_name in enumerate(f_list[:k]):
+                file_path = "/".join([data_folder_path, file_name])
+                df = pd.read_csv(file_path, header=0)
+                header_info = np.array(list(df), dtype="S")
+                f_hdf5.create_dataset("_".join(["header_info", data_chars[i]]), data=header_info)
+        print("parsed header information to hdf5 file")
+
         print("parsing .csv data to hdf5 file")
         with h5py.File(fp_hdf5, "r+") as f_hdf5:
             for i, file_name in enumerate(f_list):
                 file_path = "/".join([data_folder_path, file_name])
-                df = pd.read_csv(file_path)
+                df = pd.read_csv(file_path, header=0)
                 f_hdf5.create_dataset(file_name, data=df)
                 print("parsed file {0}/{1}".format(i+1, num_files))
